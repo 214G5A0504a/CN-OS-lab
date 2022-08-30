48fcafcdfff2c1b966868755c0e12499745e333a
214G5A0504a <112419877+214G5A0504a@users.noreply.github.com>
2022-08-30 11:38:56 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: A. anulekasai
+   Roll No: 214G5A0504
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 30-08-2022
+   Date of Execution: 30-08-2022
+*/
